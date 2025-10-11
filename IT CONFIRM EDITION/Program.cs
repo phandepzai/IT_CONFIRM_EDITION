2c887d54667cf717eeb4881a7e8c892b884a893a
nongvanphan <60602558+phandepzai@users.noreply.github.com>
2025-10-11 18:20:01 +0700

    Add files via upload

--- IT CONFIRM EDITION/Program.cs
+++ IT CONFIRM EDITION/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading; // Giữ using này để sử dụng Mutex
 
 namespace IT_CONFIRM_EDTION
 {
@@ -14,9 +15,29 @@ namespace IT_CONFIRM_EDTION
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            // Tạo Mutex với tên unique (global)
+            using (Mutex mutex = new Mutex(true, "ITConfirmEditionSingleInstanceMutex", out bool createdNew))
+            {
+                if (createdNew)
+                {
+                    // Nếu Mutex mới được tạo (chưa có instance nào chạy), tiếp tục chạy ứng dụng
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new MainForm());
+                }
+                else
+                {
+                    // Nếu Mutex đã tồn tại (instance khác đang chạy), hiển thị thông báo và thoát
+                    MessageBox.Show(
+                        "Ứng dụng đã đang chạy. Không thể mở thêm phiên bản mới.",
+                        "Thông báo",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information
+                    );
+                    // Thoát ứng dụng ngay lập tức
+                    Environment.Exit(0);
+                }
+            }
         }
     }
 }
